5f4579545405de7075b10f6d1ee7285c28c93215
JonathanRAD <jonathanrujel4@gmail.com>
2026-01-13 18:05:30 -0500

    Update project.interface.ts

--- src/app/core/models/project.interface.ts
+++ src/app/core/models/project.interface.ts
@@ -2,7 +2,7 @@ export interface Project {
   id: string;
   title: string;
   description: string;
-  technologies: string[]; // Ej: ['Angular', 'Spring Boot']
+  technologies: string[];
   imageUrl: string;
   githubUrl?: string;
   demoUrl?: string;
